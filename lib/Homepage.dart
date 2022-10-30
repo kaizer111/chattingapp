34b85013bd370b962c9201ec315199ad20a4aeb3
AnanyaSamanta09 <ananyasamanta906@gmail.com>
2022-10-30 19:39:58 +0530

    homepage

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,3 +1,4 @@
+import 'package:chattingapp/Constants/device_size.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,8 +6,57 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blue.shade50,
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Whatsagram',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/jimin.jpg'),
+                        radius: 25,
+                      )
+                    ],
+                  ),
 
+                ],
+              ),
+            ),
+
+
+            Positioned(
+              bottom: 0,
+                child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              elevation: 20,
+              child: Container(
+                height: displayHeight(context) * 0.68,
+                width: displayWidth(context),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30))),
+              ),
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
