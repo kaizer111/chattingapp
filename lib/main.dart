beb3941971e50a0c0bc93ee69e1fbfec31fa13c8
kaizer111 <mahatovishal2001@gmail.com>
2022-11-04 10:55:40 +0530

    device rientation portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'Homepage.dart';
 
 void main(){
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       home: HomePage(),
       debugShowCheckedModeBanner: false,
