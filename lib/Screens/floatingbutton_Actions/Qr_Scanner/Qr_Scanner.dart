c32c55c37b50998fa8e178b8f732368c8da03abc
kaizer111 <mahatovishal2001@gmail.com>
2023-01-10 21:40:34 +0530

    qr scanned page completed

--- lib/Screens/floatingbutton_Actions/Qr_Scanner/Qr_Scanner.dart
+++ lib/Screens/floatingbutton_Actions/Qr_Scanner/Qr_Scanner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'FoundQrCodeSuccesfully.dart';
 
 class QrScanner extends StatefulWidget {
   const QrScanner({super.key});
@@ -11,6 +12,7 @@ class QrScanner extends StatefulWidget {
 class _QrScannerState extends State<QrScanner> {
 MobileScannerController cameraController = MobileScannerController();
 bool _screenOpened = false;
+
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +66,8 @@ bool _screenOpened = false;
   void _foundBarcode(Barcode barcode, MobileScannerArguments? args) {
     /// open screen
     if (!_screenOpened) {
-      final String code = barcode.rawValue ?? "---";
+      final String code = barcode.rawValue ?? "";
+   
       debugPrint('Barcode found! $code');
       _screenOpened = true;
       Navigator.push(context, MaterialPageRoute(builder: (context) =>
@@ -74,52 +77,5 @@ bool _screenOpened = false;
 
   void _screenWasClosed() {
     _screenOpened = false;
-  }
-}
-
- 
-
-class FoundCodeScreen extends StatefulWidget {
-  final String value;
-  final Function() screenClosed;
-  const FoundCodeScreen({
-    Key? key,
-    required this.value,
-    required this.screenClosed,
-  }) : super(key: key);
-
-  @override
-  State<FoundCodeScreen> createState() => _FoundCodeScreenState();
-}
-
-class _FoundCodeScreenState extends State<FoundCodeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Found Code"),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            widget.screenClosed();
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_outlined,),
-        ),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text("Scanned Code:", style: TextStyle(fontSize: 20,),),
-              SizedBox(height: 20,),
-              Text(widget.value, style: TextStyle(fontSize: 16,),),
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
