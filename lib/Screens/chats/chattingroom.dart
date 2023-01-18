cab13037b9760e8a5fd9d8acf5db58f7486a2a86
kaizer111 <mahatovishal2001@gmail.com>
2023-01-18 21:37:23 +0530

    added comment box and listile chats can be opened

--- lib/Screens/chats/chattingroom.dart
+++ lib/Screens/chats/chattingroom.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:comment_box/comment/comment.dart';
 
 
 class ChattingRoom extends StatelessWidget {
@@ -9,9 +9,84 @@ class ChattingRoom extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue[100],
         title: Text('kakashi'),
+        actions: [
+          PopupMenuButton(onSelected: (value){
+            print(value);
+            },
+        color: Colors.blueGrey.shade100,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      itemBuilder: (BuildContext context) {
+        return [
+          PopupMenuItem(child: ListTile(
+            leading: Text('Profile',
+              style: TextStyle(
+                  fontSize: 16
+              ),
+            ),
+            onTap: () {
+            },
+          )
+          ),
+          PopupMenuItem(
+            child: ListTile(
+            leading: Text('New groups',
+              style: TextStyle(
+                  fontSize: 16
+              ),
+            ),
+            onTap: () {
+            },
+          )
+          ),
+          PopupMenuItem(child: ListTile(
+            leading: Text('Starred messages',
+              style: TextStyle(
+                  fontSize: 16
+              ),
+            ),
+            onTap: () {
+            },
+          ),
+          ),
+          PopupMenuItem(child: ListTile(
+            leading: Text('Help',
+              style: TextStyle(
+                  fontSize: 16
+              ),
+            ),
+            onTap: () {
+            },
+          )
+          ),
+
+          PopupMenuItem(child: ListTile(
+            leading: Text('Logout',
+              style: TextStyle(
+                  fontSize: 16
+              ),
+            ),
+            onTap: () {
+            },
+          )
+          ),
+
+        ];
+      }
+    ),
+        ],
       ),
-      
+      body: CommentBox(
+          backgroundColor: Colors.white,
+          //commentController: ,
+          withBorder: true,
+          textColor: Colors.black,
+          sendWidget: IconButton(onPressed: () {
+            
+          }, icon: Icon(Icons.send)),
+          child: Text('data'),
+      ),
     );
   }
 }
